01dedaab31e0965b38ac34e7d175b9d5d78820d4
Barkat ullah <barkatullah585464@gmail.com>
2023-11-08 11:42:57 +0600

    connection problem

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { ObjectId } = require('mongodb');
 
-//  MONGODB DATABASE USER PASSWORD
+
+app.use(cors());
+app.use(express.json());
+
 // console.log(process.env.DB_USER);
 // console.log(process.env.DB_PASS);
 
@@ -24,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     const assignmentCollection = client.db('assignDB').collection('assignments')
     const quizCollection = client.db('assignDB').collection('quiz')
 
@@ -158,8 +161,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -168,8 +171,7 @@ async function run() {
 run().catch(console.dir);
 
 
-app.use(cors());
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Crud is running...");
